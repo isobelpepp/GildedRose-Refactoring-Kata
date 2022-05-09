41ae57579c482c0f1fec969ca96092c6a94fbf2e
Isobel <isobelpeppiatt@yahoo.co.uk>
2022-05-09 18:38:26 +0100

    refactor for aged brie

--- ruby/lib/gilded_rose_shop.rb
+++ ruby/lib/gilded_rose_shop.rb
@@ -7,8 +7,10 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != 'Sulfuras, Hand of Ragnaros'
-        if isRegularItem(item.name)
-          processRegularItem(item)
+        if is_regular_item(item.name)
+          process_regular_item(item)
+        elsif item.name == 'Aged Brie'
+          process_aged_brie(item)
         else
           if item.quality < 50
             item.quality = item.quality + 1
@@ -26,24 +28,18 @@ class GildedRose
             end
           end
         end
-        if item.name === "Backstage passes to a TAFKAL80ETC concert" || item.name === "Aged Brie" 
+        if item.name === "Backstage passes to a TAFKAL80ETC concert"
           item.sell_in = item.sell_in - 1
         end
         if item.sell_in < 0
-          if item.name != "Aged Brie"
-            if item.name != "Backstage passes to a TAFKAL80ETC concert"
-              if item.quality > 0
-                if item.name != "Sulfuras, Hand of Ragnaros"
-                  item.quality = item.quality - 1
-                end
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              if item.name != "Sulfuras, Hand of Ragnaros"
+                item.quality = item.quality - 1
               end
-            else
-              item.quality = item.quality - item.quality
             end
           else
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
+            item.quality = item.quality - item.quality
           end
         end
       end
@@ -58,11 +54,16 @@ class GildedRose
     end
   end
 
-  def process_regular_item(item) 
+  def process_regular_item(item)
     if item.quality > 0
       item.quality -= 1
       item.quality -= 1 if item.sell_in < 0 
     end
     item.sell_in -= 1
+  end
+
+  def process_aged_brie(item)
+    item.sell_in -= 1
+    item.quality += 1 if item.quality < 50
   end
 end
